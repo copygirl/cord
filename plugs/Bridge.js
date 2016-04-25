7733727c4864d787f330f17814700bc426358159
copygirl <halfnerd.copygirl@gmail.com>
2016-04-25 10:25:17 +0200

    Rebuilding bridges

--- plugs/Bridge.js
+++ plugs/Bridge.js
@@ -3,12 +3,12 @@
 let Plug   = require("./Plug");
 let Socket = require("../sockets/Socket");
 
-let { extend, filter, any, first } = require("../utility");
+let { extend, entries, any, concat } = require("../utility");
 
 
 let defaults = {
   ignore: [ ],
-  bridges: [ ]
+  bridges: { }
 };
 
 let bridgeDefaults = {
@@ -18,79 +18,103 @@ let bridgeDefaults = {
 };
 
 
-module.exports = class Bridge extends Plug {
+let Bridge = module.exports = class Bridge extends Plug {
   
   constructor(cord, config) {
     super(cord, extend({ }, defaults, config));
     
-    this._relayMap = new Map();
+    this.bridges  = { };
+    this.channels = new Map();
     
-    for (let bridge of this.config.bridges) {
-      bridge = extend({ }, bridgeDefaults, bridge);
-      for (let channel of bridge.channels) {
-        // TODO: Verify resolve strings and make sure they're channels.
-        let array = this._relayMap.getOrAdd(channel, () => [ ]);
-        array.push(bridge);
+    this._resolveMap = new Map();
+    
+    for (let [ id, config ] of entries(this.config.bridges)) {
+      config = extend({ }, bridgeDefaults, config);
+      
+      let bridge = new Bridge.Info(this, id, config);
+      for (let resolve of bridge.resolves) {
+        let old = this._resolveMap.get(resolve);
+        if (old != null) throw new Error(
+          `Channel '${ resolve }' is in multiple ` +
+          `bridges ('${ old.id }' and '${ bridge.id }')`);
+        // TODO: Verify resolve strings and make sure they could be valid channels.
+        this._resolveMap.set(resolve, bridge);
       }
+      this.bridges[id] = bridge;
     }
   }
   
-  _makeResponse(message) {
-    // If the message was sent by the bot, re-send the same message.
-    if (message.sender.isSelf) return message.parts;
-    
-    let isAction = false;
-    let parts = Array.from(filter(message.parts, (part) =>
-      ((part == Socket.Action) ? (isAction = true, false) : true)));
-    
-    if (isAction) parts.unshift("* ", message.sender, " ");
-    else parts.unshift("<", message.sender, "> ");
-    
-    return parts;
-  }
-  
   activate() {
+    this.cord.on("newChannel", (channel) => {
+      for (let resolve of channel.resolveStrings) {
+        resolve = `${ channel.socket.id }:${ resolve }`;
+        let bridge = this._resolveMap.get(resolve);
+        if (bridge == null) continue;
+        
+        let old = this.channels.get(channel);
+        if (old != null) throw new Error(
+          `Channel '${ resolve }' active in multiple ` +
+          `bridges ('${ old.id }' and '${ bridge.id }')`);
+        
+        this.debug(`${ bridge.id } += ${ channel } (${ resolve })`);
+        channel.bridge = bridge;
+        bridge.channels.add(channel);
+        this.channels.set(channel, bridge);
+        
+        channel.on("removed", () => {
+          this.debug(`${ bridge.id } -= ${ channel }`);
+          channel.bridge = null;
+          bridge.channels.delete(channel);
+          this.channels.delete(channel);
+        });
+        
+        break;
+      }
+    });
+    
     this.cord.on("message", (message) => {
       // Make sure the message was from a user and to a channel.
       if (!(message.sender instanceof Socket.User) ||
           !(message.target instanceof Socket.Channel)) return;
       
-      let response = null;
+      let bridge = this.channels.get(message.target);
+      if ((bridge == null) || !bridge.enabled) return;
+      
       let userResolves = new Set(message.sender.resolveStrings);
       
-      // If user is ignored globally, skip this message.
-      if (any(this.config.ignore, (e) => userResolves.has(e))) return;
+      if (any(concat(this.config.ignore, bridge.config.ignore),
+              (e) => userResolves.has(e))) return;
       
-      //this.debug(`${ message.target } resolves to [ ${ message.target.resolveStrings.join(", ") } ]`);
-      for (let resolve of message.target.resolveStrings) {
-        resolve = `${ message.socket.id }:${ resolve }`;
-        let bridges = this._relayMap.get(resolve);
-        if (bridges == null) continue;
-        
-        for (let bridge of bridges) {
-          // If bridge is not enabled or user is ignored for this bridge, skip it.
-          if (!bridge.enabled || any(bridge.ignore, (e) => userResolves.has(e))) continue;
-          
-          for (let other of bridge.channels) {
-            // Don't re-send messages back to the origin channel.
-            if (other == resolve) continue;
-            
-            let [ type, targets ] = this.cord.resolve(other);
-            if ((type != "channel") || (targets.length == 0)) {
-              this.debug(`Couldn't find channel matching '${ other }'`);
-              continue;
-            }
-            
-            if (response == null) response = this._makeResponse(message);
-            for (let channel of targets) {
-              this.debug(`Relayed message from '${ message.target.socket.id }:${ message.target }' ` +
-                                           `to '${ channel.socket.id }:${ channel }'`);
-              channel.sendSilent(...response);
-            }
-          }
-        }
+      let parts = message.parts;
+      if (!message.sender.isSelf) {
+        let isAction = false;
+        parts = message.augmentClone(Socket.Action, () => (isAction = true, null));
+        if (isAction) parts.unshift("* ", message.sender, " ");
+        else parts.unshift("<", message.sender, "> ");
       }
+      
+      for (let channel of bridge.channels)
+        if (channel !== message.target)
+          channel.sendSilent(...parts);
     });
   }
+  
+};
+
+Bridge.Info = class BridgeInfo {
+  
+  constructor(plug, id, config) {
+    this.plug   = plug;
+    this.id     = id;
+    this.config = config;
+    
+    this.channels = new Set();
+    this.resolves = new Set(config.channels);
+  }
+  
+  get enabled() { return this.config.enabled; }
+  set enabled(value) { this.config.enabled = value; }
+  
+  toString() { return `bridge:${ this.id }`; }
   
 };
